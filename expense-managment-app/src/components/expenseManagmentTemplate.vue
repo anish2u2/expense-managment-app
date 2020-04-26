7ec1a2868a1d0bf1050c1c1b793e5d1661930c78
Anish Singh <Anish.Singh@adesa.com>
2020-04-26 20:41:48 +0530

    Expense Managment template added.

---
+++ expense-managment-app/src/components/expenseManagmentTemplate.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div id="menus">
+        <menuLinks linkClass="menuLink" linkName="fetchExpenses" menuTitle="Read All Expenses"/>
+        <menuLinks linkClass="menuLink" linkName="createExpenses" menuTitle="Create Expenses"/>
+        <menuLinks linkClass="menuLink" linkName="deleteExpenses" menuTitle="Delete Expenses"/>
+        <menuLinks linkClass="menuLink" linkName="updateExpenses" menuTitle="Update Expenses"/>
+    </div>
+</template>
+
+<script>
+
+import menuLinks from "./menu";
+
+export default {
+  name: "expenseManagmentTemplate",
+  components: {
+    menuLinks
+  }
+};
+</script>
---
+++ expense-managment-app/src/components/menu.vue
@@ -0,0 +1,30 @@
+<template>
+    <div :class="linkClass" :linkName="linkName" @click="onMenuClick">{{menuTitle}}</div>
+</template>
+
+<script>
+    export default {
+        name: "menuLinks" ,
+        props:{
+            linkClass:{
+                type: String
+                },
+            linkName: {
+                type: String
+                },
+            menuTitle: {type:String}
+        },
+        methods:{
+            onMenuClick:function(){
+                console.log("Clicked");
+            }
+        }
+
+    };
+</script>
+
+<style>
+     .linkClass {
+
+    }
+</style>
